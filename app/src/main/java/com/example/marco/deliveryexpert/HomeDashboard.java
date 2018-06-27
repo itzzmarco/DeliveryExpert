648a9b1fe29c945dd96d4b53a85208810e4879fd
itzzmarco <itzzmarco@gmail.com>
2018-06-26 22:35:23 -0600

    Hello

--- app/src/main/java/com/example/marco/deliveryexpert/HomeDashboard.java
+++ app/src/main/java/com/example/marco/deliveryexpert/HomeDashboard.java
@@ -6,16 +6,16 @@ import android.support.v7.widget.CardView;
 import android.view.View;
 
 public class HomeDashboard extends AppCompatActivity implements View.OnClickListener{
-    private CardView bankCard, ideasCard, addCard, linksCard, wifiCard:
+    private CardView bankCard, ideasCard, addCard, linksCard, wifiCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dashboard);
-        bankCard = (CardView) findViewById(R.id.bankcardId):
-        ideasCard = (CardView) findViewById(R.id.ideas_card):
-        addCard = (CardView) findViewById(R.id.add_card):
-        linksCard = (CardView) findViewById(R.id.links_card):
-        wifiCard = (CardView) findViewById(R.id.wifi_card):
+        bankCard = (CardView) findViewById(R.id.bank_card);
+        ideasCard = (CardView) findViewById(R.id.ideas_card);
+        addCard = (CardView) findViewById(R.id.add_card);
+        linksCard = (CardView) findViewById(R.id.links_card);
+        wifiCard = (CardView) findViewById(R.id.wifi_card);
 
     }
 
