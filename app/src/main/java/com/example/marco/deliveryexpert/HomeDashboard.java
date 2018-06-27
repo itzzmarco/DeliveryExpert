b8a69b03535941b138b95476a7fcd7e602bc0069
itzzmarco <itzzmarco@gmail.com>
2018-06-27 04:04:56 -0600

    Hello

--- app/src/main/java/com/example/marco/deliveryexpert/HomeDashboard.java
+++ app/src/main/java/com/example/marco/deliveryexpert/HomeDashboard.java
@@ -1,5 +1,6 @@
 package com.example.marco.deliveryexpert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -11,16 +12,34 @@ public class HomeDashboard extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dashboard);
+        //defining Cards
         bankCard = (CardView) findViewById(R.id.bank_card);
         ideasCard = (CardView) findViewById(R.id.ideas_card);
         addCard = (CardView) findViewById(R.id.add_card);
         linksCard = (CardView) findViewById(R.id.links_card);
         wifiCard = (CardView) findViewById(R.id.wifi_card);
+        // Add Click listener to the cards
+        bankCard.setOnClickListener(this);
+        ideasCard.setOnClickListener(this);
+        addCard.setOnClickListener(this);
+        linksCard.setOnClickListener(this);
+        wifiCard.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        Intent i ;
 
-    }
+        switch (view.getId()){
+                case R.id.bank_card : i = new Intent(this, Bank.class);startActivity(i);break;
+                case R.id.add_card : i = new Intent(this, Add.class);startActivity(i);break;
+                case R.id.links_card : i = new Intent(this, Links.class);startActivity(i);break;
+                case R.id.ideas_card : i = new Intent(this, Ideas.class);startActivity(i);break;
+                case R.id.wifi_card : i = new Intent(this, Wifi.class);startActivity(i);break;
+                default:break ;
+         }
+
+
+        }
 }
