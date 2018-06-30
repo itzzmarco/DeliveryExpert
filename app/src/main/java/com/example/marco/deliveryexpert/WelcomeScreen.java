dd3087217a0b790e8508609db2d58a1e7c50b032
itzzmarco <itzzmarco@gmail.com>
2018-06-30 02:29:12 -0600

    hello4

--- app/src/main/java/com/example/marco/deliveryexpert/WelcomeScreen.java
+++ app/src/main/java/com/example/marco/deliveryexpert/WelcomeScreen.java
@@ -4,17 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+
 
 public class WelcomeScreen extends AppCompatActivity {
     // Add Enter Button Press to bring up HomeDashboard
     LinearLayout l1,l2;
     Button btnsub;
-    Animation uptodown,downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +24,8 @@ public class WelcomeScreen extends AppCompatActivity {
                 openHomeDashboard();
             }
         });
-        l1 = (LinearLayout) findViewById(R.id.l1);
-        l2 = (LinearLayout) findViewById(R.id.l2);
-        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
-        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
-        l1.setAnimation(uptodown);
-        l2.setAnimation(downtoup);
+
+
 
     }
 
@@ -44,6 +37,6 @@ public class WelcomeScreen extends AppCompatActivity {
 
 }
 
-    // Do something in response to button click
+
 
 
